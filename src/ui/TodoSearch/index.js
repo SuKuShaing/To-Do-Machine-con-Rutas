a5810273818ebb7dd670c14fd497be8a337bcd49
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-04-25 14:27:09 -0400

    agrega funcionalidad de búsqueda en la URL para el componente TodoSearch

--- src/ui/TodoSearch/index.js
+++ src/ui/TodoSearch/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./TodoSearch.css";
 import { TodoContext } from "../todoContext";
-
+import { useSearchParams } from "react-router-dom";
 
 function TodoSearch() {
 	// const [state, setState] = React.useState('Valor inicial del estado');
@@ -11,16 +11,37 @@ function TodoSearch() {
 	// los estados van de padres a hijos y no al revés
 
 	const { searchValue, setSearchValue } = React.useContext(TodoContext);
+	let [searchParams, setSearchParams] = useSearchParams();
+	console.log("🚀 ~ HomePage ~ searchParams:", searchParams);
+
+	// Inicializa el estado con el parámetro "Search" de la URL (si existe)
+	React.useEffect(() => {
+		const query = searchParams.get("Search") || "";
+		setSearchValue(query);
+	}, [searchParams, setSearchValue]);
+
+	const handleChange = (event) => {
+		const newValue = event.target.value;
+		setSearchValue(newValue);
+
+		// Actualiza la URL: si hay búsqueda, agrega o actualiza el parámetro "Search"; de lo contrario, lo remueve
+		if (newValue) {
+			setSearchParams({ Search: newValue });
+		} else {
+			setSearchParams({});
+		}
+	};
 
 	return (
 		<input
 			placeholder="Buscar ToDo"
 			className="input-TodoSearch"
 			value={searchValue} // valor que van a ver los usuarios
-			onChange={(event) => {
-					console.log(event.target.value);
-					setSearchValue(event.target.value);
-				}}
+			onChange={handleChange} // función que se ejecuta cuando el usuario escribe en el input
+			// onChange={(event) => {
+			// 	console.log(event.target.value);
+			// 	setSearchValue(event.target.value);
+			// }}
 		/>
 	);
 }
